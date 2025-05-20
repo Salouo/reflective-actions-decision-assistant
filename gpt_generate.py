b81fe1c20bda91387d3c60dd6b0ded9ca4a5c9db
Salouo <chenjunfenggo@gmail.com>
2025-05-20 16:27:35 +0900

    code modification

--- gpt_generate.py
+++ gpt_generate.py
@@ -1,15 +1,14 @@
 """
-This module is to create a prompt as input of LLM, and obtain output from LLM which is stored as pickle file.
-
-gpt4o
+This module is to create a prompt as input of GPT-4o, and obtain output from LLM which is stored as pickle file.
 """
-
-
 import json
 import os
 import my_utils
 import time
-import tqdm
+import random
+import glob
+import base64
+import openai
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -20,15 +19,20 @@ loaded = load_dotenv()
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
-def main():
-    with open('data/system_prompt_v1.txt') as f:
-        system_message = f.read()
-    with open('data/prompt.json', "r") as f:
-        user_agent_prompts_base = [json.loads(l.strip()) for l in f]
-    
+def jpg_to_data_uri(path: str) -> str:
+    '''
+    This function is used to convert *.jpg to urls which can be feed into GPT-4o.
+    '''
+    with open(path, "rb") as f:
+        b64 = base64.b64encode(f.read()).decode("ascii")
+    return f"data:image/jpeg;base64,{b64}"
+
+
+def main(system_message, user_agent_prompts_base, image_urls):
+    n_scenarios = len(image_urls)
     running_time = 0
     outputs = []
-    for user_agent_prompt_base in tqdm.tqdm(user_agent_prompts_base):
+    for idx, (user_agent_prompt_base, image_url) in enumerate(zip(user_agent_prompts_base, image_urls)):
         ref_user_prompt = "【ユーザの状況】\n"
         ref_user_prompt += f"ユーザの発話: {user_agent_prompt_base['ref_uttr']}\n"
         ref_user_prompt += f"ユーザの位置: {user_agent_prompt_base['ref_position']}\n"
@@ -45,13 +49,18 @@ def main():
         user_prompt += f"ユーザの近くにある物: {user_agent_prompt_base['near_objs']}"
 
         # CoT
-        user_prompt += "日常生活の常識に基づいて、順を追って考え、答えを出してください。"
+        # user_prompt += "日常生活の常識に基づいて、順を追って考え、答えを出してください。"
 
         # conversation template (one shot)
         messages = [
             {"role": "system", "content": system_message},
             {"role": "user", "content": ref_user_prompt},
             {"role": "assistant", "content": ref_agent_prompt},
+            {"role": "user",
+            "content": [
+                {"type": "text", "text": "以下の写真をご覧ください。"},
+                {"type":"image_url", "image_url": {"url": f"{image_url}"}}
+            ]},
             {"role": "user", "content": user_prompt}
         ]
         start_time = time.time()
@@ -67,13 +76,25 @@ def main():
         )
         end_time = time.time()
         running_time += end_time - start_time
-
+        response = completion.choices[0].message.content.strip().split('\n')
+        print(f"=== Example [{idx + 1}/{n_scenarios}] ===\n{response}\n")
         # Convert the current iteration output from LLM to a list
-        outputs.append(completion.choices[0].message.content.strip().split('\n'))
+        outputs.append(response)
 
-    my_utils.pickle_write(outputs, 'llm_outputs/gpt4o_20241120_AtLeastOneAction_CoT.pickle', overwrite=True)
-    print(f'実行時間: {(running_time) / 60:.3f}min')
+    my_utils.pickle_write(outputs, 'model_outputs/vlms/gpt4o/anno-and-img.pkl', overwrite=True)
+    print(f'running time: {(running_time) / 60:.3f}min')
+
 
 if __name__ == '__main__':
-    main()
-    print('Done')
+    with open('prompts/system_prompt_anno_img.txt') as f:
+        system_message = f.read()
+    with open('data/prompt.jsonl', "r") as f:
+        user_agent_prompts_base = [json.loads(l.strip()) for l in f]
+    # Load the revised images
+    raw_image_urls = glob.glob('data/revised_images/*.jpg')
+    image_urls = [jpg_to_data_uri(raw_image_url) for raw_image_url in raw_image_urls]
+
+    ref_image_urls = random.sample(image_urls, k=len(image_urls))
+
+    main(system_message, user_agent_prompts_base, image_urls)
+    print('Done!')
--- sarashina2-vision_generate.py
+++ sarashina2-vision_generate.py
@@ -1,11 +1,14 @@
 import json
 import pickle
 import my_utils
+import time
 from PIL import Image
 from transformers import AutoModelForCausalLM, AutoProcessor
 
 
 def main(system_prompt, user_agent_prompts_base, revised_images):
+    # Conuting running time
+    running_time = 0
     # Define model path
     model_path = "sbintuitions/sarashina2-vision-14b"
     cache_dir = "/gs/bs/tga-c-ird-lab/chen/considerate-robot/models_cache/sarashina2-vision-14b"
@@ -51,11 +54,13 @@ def main(system_prompt, user_agent_prompts_base, revised_images):
 
         ### Human: `user_prompt`
         ### Assistant:"""
-        
+        start_time = time.time()
+
         inputs = processor(
             text=[text_prompt],
-            # images=[image.convert('RGB')],
-            images=None,
+            ### Input image or not
+            images=[image.convert('RGB')],
+            # images=None,
             padding=True,
             return_tensors="pt"
         )
@@ -76,15 +81,27 @@ def main(system_prompt, user_agent_prompts_base, revised_images):
         output_text = processor.batch_decode(
             generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
         )
-        response = output_text[0]
-        print(f"=== Example [{idx + 1}/{n_senarios}] ===\n{response}\n")
 
+        end_time = time.time()
+        running_time += end_time - start_time
+
+        # Obtain the response from model
+        response = output_text[0]
+
+        # Sometimes the last character of responses is `[`.
+        if response.endswith('['):
+            response = response[:-1]
+        response = response.strip().split('\n')
         # Sometimes responses are not followed by [回答終了].
-        if response.strip().split('\n')[-1] == "[回答終了]": 
-            outputs.append(response.strip().split('\n')[:-1])
-        else:
-            outputs.append(response.strip().split('\n'))
-    my_utils.pickle_write(outputs, 'model_outputs/vlms/sarashina2-vision/anno-only.pkl', overwrite=False)
+        if response[-1] == "[回答終了]": 
+            response = response[:-1]
+        # Print the output at current iteration
+        print(f"=== Example [{idx + 1}/{n_senarios}] ===\n{response}\n")
+        # Collect the output at current iteration
+        outputs.append(response)
+    
+    my_utils.pickle_write(outputs, 'model_outputs/vlms/sarashina2-vision/anno-and-img.pkl', overwrite=True)
+    print(f'running time: {(running_time) / 60:.3f}min')
 
 
 if __name__ == "__main__":
@@ -94,5 +111,5 @@ if __name__ == "__main__":
         user_agent_prompts_base = [json.loads(l.strip()) for l in f]
     with open('data/revised_images.pkl', 'rb') as f:
         revised_images = pickle.load(f)
-
+    
     main(system_prompt, user_agent_prompts_base, revised_images)
